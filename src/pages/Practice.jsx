28038fc6b00e87375a5e599a023f5121abeafa05
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-24 21:16:08 +0600

    Create practice page

---
+++ src/pages/Practice.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Player from "../components/Player";
+import Form from "../components/Form";
+
+const Practice = () => (
+  <div>
+    <Player word="Extravagance" />
+    <Form />
+  </div>
+);
+
+export default Practice;
