f86ecc39a38c2ba62b8334742b305e61828e13c5
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-27 18:38:28 +0600

    Implement practicising the most difficult words

--- src/pages/Practice.jsx
+++ src/pages/Practice.jsx
@@ -6,14 +6,34 @@ import Form from "../components/Form";
 import styles from "./Practice.module.css";
 
 const Practice = () => {
-  const { words } = useContext(StoreContext);
-  const [option, setOption] = useState("ALL"); // ALL/FILTERED
+  const { words, stats } = useContext(StoreContext);
+  const [option, setOption] = useState("RANDOM"); // RANDOM/DIFFICULTY
   const [queue, setQueue] = useState([]);
   const [practiceIndex, setPracticeIndex] = useState(0);
 
   useEffect(() => {
-    if (option === "FILTERED") setQueue([]);
-    else if (option === "ALL") setQueue(words);
+    if (option === "RANDOM")
+      setQueue([].concat(words).sort(() => Math.random() - 0.5));
+    else if (option === "DIFFICULTY") {
+      const wordListWithPrecision = words.map(word => {
+        let precision = 0;
+
+        if (stats[word]) {
+          const statsOnWord = stats[word];
+          precision =
+            statsOnWord.filter(({ isCorrect }) => isCorrect).length /
+            statsOnWord.length;
+        }
+
+        return { word, precision };
+      });
+
+      setQueue(
+        wordListWithPrecision
+          .sort((a, b) => a.precision - b.precision)
+          .map(({ word }) => word)
+      );
+    }
 
     setPracticeIndex(0);
   }, [option, words]);
@@ -30,14 +50,14 @@ const Practice = () => {
         </p>
 
         <label>
-          <span>Practice </span>
+          <span>Sort By </span>
           <select
             value={option}
             className={styles.select}
             onChange={e => setOption(e.target.value)}
           >
-            <option value="ALL">All</option>
-            <option value="FILTERED">Filtered</option>
+            <option value="RANDOM">Random</option>
+            <option value="DIFFICULTY">Difficulty</option>
           </select>
         </label>
       </div>
