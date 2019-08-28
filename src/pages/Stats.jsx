ef35df6f39c3c6f87831c47750201028da27ec55
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-28 19:08:31 +0600

    Add UI for search and sorting on stats page

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -3,6 +3,7 @@ import React, { useContext } from "react";
 import StoreContext from "../store";
 
 import styles from "./Stats.module.css";
+import formStyles from "../components/Form.module.css";
 import StatOnWord from "../components/StatOnWord";
 
 const Stats = () => {
@@ -10,6 +11,28 @@ const Stats = () => {
 
   return (
     <div>
+      <aside className={styles.optionsPanel}>
+        <div>
+          <h3>Search by Word</h3>
+
+          <input
+            type="text"
+            className={formStyles.input}
+            placeholder="Search..."
+          />
+        </div>
+        <div className={styles.sortSection}>
+          <h3>Sort By</h3>
+
+          <select className={formStyles.input}>
+            <option value="PRECISION">Precision</option>
+            <option value="AVG_SPEED">Avg. Speed</option>
+            <option value="ATTEMPTS">Attemps</option>
+            <option value="CORRECT_ATTEMPTS">Correct</option>
+          </select>
+        </div>
+      </aside>
+
       <ul className={styles.list}>
         {words
           .map(word => {
