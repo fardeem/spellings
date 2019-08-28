902d343e8095f7fa7b224ab5d70da6ea6c245553
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-28 22:40:45 +0600

    Don't show stats on words that haven't been attempted yet

--- src/components/StatOnWord.jsx
+++ src/components/StatOnWord.jsx
@@ -11,24 +11,28 @@ const StatOnWord = ({
 }) => (
   <>
     <p>{word}</p>
-    <ul className={styles.detailsList}>
-      <li>
-        <span>Attempts</span>
-        <span>{attempts}</span>
-      </li>
-      <li>
-        <span>Correct</span>
-        <span>{correctAttempts}</span>
-      </li>
-      <li>
-        <span>Precision</span>
-        <span>{Math.round(precision * 100)}</span>
-      </li>
-      <li>
-        <span>Avg. Speed</span>
-        <span>{Math.round(avgSpeed / 1000)}</span>
-      </li>
-    </ul>
+    {attempts > 0 ? (
+      <ul className={styles.detailsList}>
+        <li>
+          <span>Attempts</span>
+          <span>{attempts}</span>
+        </li>
+        <li>
+          <span>Correct</span>
+          <span>{correctAttempts}</span>
+        </li>
+        <li>
+          <span>Precision</span>
+          <span>{Math.round(precision * 100)}</span>
+        </li>
+        <li>
+          <span>Avg. Speed</span>
+          <span>{Math.round(avgSpeed / 1000)}</span>
+        </li>
+      </ul>
+    ) : (
+      <div className={styles.empty}>No attempts yet</div>
+    )}
   </>
 );
 
