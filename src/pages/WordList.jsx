c95cf02574672fb10204316366e716b0418f894b
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-28 18:05:24 +0600

    Add list of words and form to add new word on word list page

--- src/pages/WordList.jsx
+++ src/pages/WordList.jsx
@@ -1,6 +1,55 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+
+import StoreContext from "../store";
+
+import styles from "./WordList.module.css";
+import formStyles from "../components/Form.module.css";
+
 const WordList = () => {
-  return <h1>Hello Words</h1>;
+  const { words, dispatch } = useContext(StoreContext);
+  const [newWord, setNewWord] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!words.includes(newWord.trim().toLowerCase())) {
+      dispatch({
+        type: "ADD_WORD",
+        value: { word: newWord }
+      });
+    }
+
+    setNewWord("");
+  }
+
+  return (
+    <article className={styles.container}>
+      <div>
+        <h1 className={styles.heading}>Add Word</h1>
+
+        <form onSubmit={handleSubmit} className={formStyles.form}>
+          <input
+            type="text"
+            value={newWord}
+            onChange={e => setNewWord(e.target.value)}
+            className={formStyles.input}
+          />
+          <input type="submit" className={formStyles.btn} />
+        </form>
+      </div>
+
+      <div>
+        <h1 className={styles.heading}>Word List</h1>
+        <ol className={styles.list}>
+          {words.map((word, i) => (
+            <li key={i} className={styles.listItem}>
+              {word}
+            </li>
+          ))}
+        </ol>
+      </div>
+    </article>
+  );
 };
 
 export default WordList;
